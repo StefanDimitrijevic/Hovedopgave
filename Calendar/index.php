c2a8bf546da724177b73ef9a6f7fd69e6264b929
Stefan Dimitrijevic <cph-sd77@cphbusiness.dk>
2019-05-08 00:35:20 +0200

    implemented correct javascript

--- Calendar/index.php
+++ Calendar/index.php
@@ -12,37 +12,37 @@
 </head>
 <body>
     <div class="container">
-            <div class="wrap">
-                <div class="door">
-                    <p class="number-text">1</p>
-                </div>
-                <div class="inside">
-                </div>
+        <div class="wrap">
+            <div class="door">
+                <p class="number-text">1</p>
             </div>
-            
-            <div class="wrap">
-                <div class="door">
-                    <p class="number-text">2</p>
-                </div>
-                <div class="inside">
-                </div>
+            <div class="inside" id="inside-one">
             </div>
-    
-            <div class="wrap">
-                <div class="door">
-                    <p class="number-text">3</p>
-                </div>
-                <div class="inside">
-                </div>
+        </div>
+        
+        <div class="wrap">
+            <div class="door">
+                <p class="number-text">2</p>
             </div>
-    
-            <div class="wrap">
-                <div class="door">
-                    <p class="number-text">4</p>
-                </div>
-                <div class="inside">
-                </div>
+            <div class="inside" id="inside-two">
             </div>
+        </div>
+
+        <div class="wrap">
+            <div class="door">
+                <p class="number-text">3</p>
+            </div>
+            <div class="inside" id="inside-three">
+            </div>
+        </div>
+
+        <div class="wrap">
+            <div class="door">
+                <p class="number-text">4</p>
+            </div>
+            <div class="inside" id="inside-four">
+            </div>
+        </div>
 
         <?php 
         include_once "dbcon.php";
@@ -52,17 +52,88 @@
         $stmt->execute();
         while($stmt->fetch()) { ?>
         
-        <div class="popup">
+        <div class="popup" id="first-popup">
             <div class="hm">
                 <p class="exit">x</p>
                 <h1><?=$title?></h1>
                 <h3><?=$desc?></h3>
                 <img class="popup-image" src="uploads/<?=$fullName?>" alt="">
-                <a href="http://<?=$siteLink?>" target="_blank">Tryk for at gå til produktet</a>
+               <button>
+                    <a href="http://<?=$siteLink?>" target="_blank">Tryk for at gå til produktet</a>
+               </button>
             </div>
         </div> 
         
-        <?php } ?> 
+        <?php } ?>
+
+
+        <?php 
+        include_once "dbcon.php";
+        $sql = "SELECT titleContent, descContent, linkContent, imgNameContent FROM content WHERE calendarDoors_idCalendarDoors=2 ORDER BY idContent DESC";
+        $stmt = $link->prepare($sql);
+        $stmt->bind_result($title, $desc, $siteLink, $fullName);
+        $stmt->execute();
+        while($stmt->fetch()) { ?>
+        
+        <div class="popup" id="second-popup">
+            <div class="hm">
+                <p class="exit">x</p>
+                <h1><?=$title?></h1>
+                <h3><?=$desc?></h3>
+                <img class="popup-image" src="uploads/<?=$fullName?>" alt="">
+               <button>
+                    <a href="http://<?=$siteLink?>" target="_blank">Tryk for at gå til produktet</a>
+               </button>
+            </div>
+        </div> 
+        
+        <?php } ?>
+
+
+        <?php 
+        include_once "dbcon.php";
+        $sql = "SELECT titleContent, descContent, linkContent, imgNameContent FROM content WHERE calendarDoors_idCalendarDoors=3 ORDER BY idContent DESC";
+        $stmt = $link->prepare($sql);
+        $stmt->bind_result($title, $desc, $siteLink, $fullName);
+        $stmt->execute();
+        while($stmt->fetch()) { ?>
+        
+        <div class="popup" id="third-popup">
+            <div class="hm">
+                <p class="exit">x</p>
+                <h1><?=$title?></h1>
+                <h3><?=$desc?></h3>
+                <img class="popup-image" src="uploads/<?=$fullName?>" alt="">
+               <button>
+                    <a href="http://<?=$siteLink?>" target="_blank">Tryk for at gå til produktet</a>
+               </button>
+            </div>
+        </div> 
+        
+        <?php } ?>
+
+
+        <?php 
+        include_once "dbcon.php";
+        $sql = "SELECT titleContent, descContent, linkContent, imgNameContent FROM content WHERE calendarDoors_idCalendarDoors=4 ORDER BY idContent DESC";
+        $stmt = $link->prepare($sql);
+        $stmt->bind_result($title, $desc, $siteLink, $fullName);
+        $stmt->execute();
+        while($stmt->fetch()) { ?>
+        
+        <div class="popup" id="fourth-popup">
+            <div class="hm">
+                <p class="exit">x</p>
+                <h1><?=$title?></h1>
+                <h3><?=$desc?></h3>
+                <img class="popup-image" src="uploads/<?=$fullName?>" alt="">
+               <button>
+                    <a href="http://<?=$siteLink?>" target="_blank">Tryk for at gå til produktet</a>
+               </button>
+            </div>
+        </div> 
+        
+        <?php } ?>
 
         <?php
             if(isset($_SESSION['users_id'])){
--- Calendar/js/doors.js
+++ Calendar/js/doors.js
@@ -1,23 +1,47 @@
+// GLOBAL VARIABLES
 var door = document.querySelectorAll('.door');
-var inside = document.querySelectorAll('.inside');
-var popup = document.querySelector('.popup');
+
+var firstInside = document.getElementById('inside-one');
+var firstPopup = document.getElementById('first-popup');
+var secondInside = document.getElementById('inside-two');
+var secondPopup = document.getElementById('second-popup');
+var thirdInside = document.getElementById('inside-three');
+var thirdPopup = document.getElementById('third-popup');
+var fourthInside = document.getElementById('inside-four');
+var fourthPopup = document.getElementById('fourth-popup');
+
 var exit = document.querySelectorAll('.exit');
 
-door.forEach( (item) => {
-    item.addEventListener('click', function(){
-        item.classList.toggle('open');
+// Looping through all doors and adding an EventListener to them
+door.forEach( (doors) => {
+    doors.addEventListener('click', function(){
+        doors.classList.toggle('open');
     });
 });
 
-inside.forEach( (insides) => {
-    insides.addEventListener('click', function(){
-        popup.classList.toggle('modal');
+    firstInside.addEventListener('click', () => {
+        firstPopup.classList.toggle('modal');
     });
-});
 
+    secondInside.addEventListener('click', () => {
+        secondPopup.classList.toggle('modal');
+    });
+
+    thirdInside.addEventListener('click', () => {
+        thirdPopup.classList.toggle('modal');
+    });
+
+    fourthInside.addEventListener('click', () => {
+        fourthPopup.classList.toggle('modal');
+    });
+
+// Looping through all exits and adding an EventListener to them
 exit.forEach( (exits) => {
     exits.addEventListener('click', function() {
-        popup.classList.toggle('modal');
+        firstPopup.classList.remove('modal');
+        secondPopup.classList.remove('modal');
+        thirdPopup.classList.remove('modal');
+        fourthPopup.classList.remove('modal');
         door.forEach( (doors) => {
             doors.classList.remove('open');
         });
